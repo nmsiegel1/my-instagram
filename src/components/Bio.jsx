37c4d71b9174d8846cdd1c2aa92f6ca731c2b54a
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-12-05 21:18:08 -0500

    add indexedDB functionality, use Live Query to add photos to gallery, add delete photo functionality

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,25 +1,47 @@
 import profileIcon from '../assets/profileIcon.svg'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import getPhotoUrl from 'get-photo-url'
+import { db } from '../dexie'
+
 const Bio = () => {
      const [userDetails, setUserDetails] = useState({
         name: "Nina",
         about: 'Learning react :)'
      })
      const [editFormIsOpen, setEditFormIsOpen] = useState(false)
+     const [profilePhoto, setProfilePhoto] = useState(profileIcon)
 
-    const updateUserDetails = (event) => {
+     useEffect(() => {
+        const setDataFromDb = async () => {
+            const userDetailsFromDb = await db.bio.get('info')
+            const profilePhotoFromDb = await db.bio.get('profilePhoto')
+            userDetailsFromDb && setUserDetails(userDetailsFromDb)
+            profilePhotoFromDb && setProfilePhoto(profilePhotoFromDb)
+        }
+        setDataFromDb()
+     })
+
+    const updateUserDetails = async (event) => {
         event.preventDefault()
-        setUserDetails({
+        const objectData = {
             name: event.target.nameOfUser.value,
             about: event.target.aboutUser.value
-        })
+        }
+        setUserDetails(objectData)
+        db.bio.put(objectData, 'info')
         setEditFormIsOpen(false)
+    }
+
+    const updeateProfilePhoto = async() => {
+        const newProfilePhoto = await getPhotoUrl('#profilePhotoInput')
+        setProfilePhoto(newProfilePhoto)
+        await db.bio.put(newProfilePhoto, 'profilePhoto')
     }
 
     const editForm = (
     <form className='edit-bio-form' onSubmit={(e) => updateUserDetails(e)}>
-        <input type='text' id='' name="nameOfUser" placeholder='Your name' />
-        <input type='text' id='' name="aboutUser" placeholder='About you' />
+        <input type='text' id='' name="nameOfUser" defaultValue={userDetails?.name} placeholder='Your name' />
+        <input type='text' id='' name="aboutUser" defaultValue={userDetails?.about}placeholder='About you' />
         <br />
         <button type="button" id="" className='cancel-button' onClick={() => setEditFormIsOpen(false)}>Cancel</button>
         <button type='submit'>Save</button>
@@ -28,14 +50,20 @@ const Bio = () => {
 )
 
     const editButton = <button onClick={() => setEditFormIsOpen(true)}>Edit</button>
+
+
     return (
 <section className="bio">
-    <div className="profile-photo" role='button' title='Click to edit photo'>
-        <img src={profileIcon} alt='profile' />
-    </div>
+    <input type='file' accept='image/*' name='photo' id='profilePhotoInput' />
+    <label htmlFor='profilePhotoInput' onClick={updeateProfilePhoto}>
+        <div className='profile-photo' role='button' title='Click to edit photo'>
+            <img src={profilePhoto} alt='profile' />
+        </div>
+    </label>
+
     <div className='profile-info'>
         <p className='name'>{userDetails.name}</p>
-        <p className='about'>{userDetails.about}</p>  
+        <p className='about'>{userDetails.about}</p>
         {editFormIsOpen ? editForm: editButton}
     </div>
 </section>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,38 +1,35 @@
- import pexels1 from '../assets/pexels1.jpeg'
- import pexels2 from '../assets/pexels2.jpeg'
- import pexels3 from '../assets/pexels3.jpeg'
- import pexels4 from '../assets/pexels4.jpeg'
- import pexels5 from '../assets/pexels5.jpeg'
-
+import getPhotoUrl from "get-photo-url"
+import { useLiveQuery } from 'dexie-react-hooks'
+import { db } from '../dexie'
+ 
  const Gallery = () => {
+    const allPhotos = useLiveQuery(() => db.gallery.toArray(), [])
+
+    const addPhoto = async () => {
+        db.gallery.add({
+            url: await getPhotoUrl('#addPhotoInput')
+        })
+    }
+
+    const removePhoto = (id) => {
+        db.gallery.delete(id)
+    }
+
     return (
         <>
         <input type='file' name='photo' id='addPhotoInput' />
-        <label htmlFor="addPhotoInput">
+        <label htmlFor="addPhotoInput" onClick={addPhoto}>
             <i className="add-photo-button fas fa-plus-square"></i>
         </label>
 
         <section className='gallery'>
-    <div className="item">
-        <img src={pexels1} className='item-image' alt='' />
-        <button className='delete-button'>Delete</button>
+            {!allPhotos && <p>Loading...</p>}
+            {allPhotos?.map((photo) => (
+                <div className="item" key={photo.id}>
+        <img src={photo.url} className='item-image' alt='' />
+        <button className='delete-button' onClick={() => removePhoto(photo.id)}>Delete</button>
     </div>
-    <div className="item">
-        <img src={pexels2} className='item-image' alt='' />
-        <button className='delete-button'>Delete</button>
-    </div>
-    <div className="item">
-        <img src={pexels3} className='item-image' alt='' />
-        <button className='delete-button'>Delete</button>
-    </div>
-    <div className="item">
-        <img src={pexels4} className='item-image' alt='' />
-        <button className='delete-button'>Delete</button>
-    </div>
-    <div className="item">
-        <img src={pexels5} className='item-image' alt='' />
-        <button className='delete-button'>Delete</button>
-    </div>
+        ))}
         </section>
         </>
     )
